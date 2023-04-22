b91e8f6d6b74d4db2f2916bbe7405d5f127d7c98
moeyad7 <mohamed.eyad2612@gmail.com>
2023-04-22 18:01:24 +0200

    video 22 folder 10

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
 
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
-import '../providers/cart.dart';
+import './cart.dart';
 
 class OrderItem {
   final String id;
@@ -26,11 +26,42 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
+  Future<void> fetchAndSetOrders() async {
+    final url = Uri.parse(
+        'https://flutter-shop-app-b1bc8-default-rtdb.firebaseio.com/orders.json');
+    final response = await http.get(url);
+    final List<OrderItem> loadedOrders = [];
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+    if (extractedData == null) {
+      return;
+    }
+    extractedData.forEach((orderId, orderData) {
+      loadedOrders.add(
+        OrderItem(
+          id: orderId,
+          amount: orderData['amount'],
+          dateTime: DateTime.parse(orderData['dateTime']),
+          products: (orderData['products'] as List<dynamic>)
+              .map(
+                (item) => CartItem(
+                  id: item['id'],
+                  price: item['price'],
+                  quantity: item['quantity'],
+                  title: item['title'],
+                ),
+              )
+              .toList(),
+        ),
+      );
+    });
+    _orders = loadedOrders.reversed.toList();
+    notifyListeners();
+  }
+
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
     final url = Uri.parse(
         'https://flutter-shop-app-b1bc8-default-rtdb.firebaseio.com/orders.json');
     final timestamp = DateTime.now();
-
     final response = await http.post(
       url,
       body: json.encode({
@@ -47,13 +78,14 @@ class Orders with ChangeNotifier {
       }),
     );
     _orders.insert(
-        0,
-        OrderItem(
-          id: json.decode(response.body)['name'],
-          amount: total,
-          products: cartProducts,
-          dateTime: timestamp,
-        ));
+      0,
+      OrderItem(
+        id: json.decode(response.body)['name'],
+        amount: total,
+        dateTime: timestamp,
+        products: cartProducts,
+      ),
+    );
     notifyListeners();
   }
 }
