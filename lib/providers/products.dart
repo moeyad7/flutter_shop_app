e00877fd78c5831b3883bb5a2b21f32d35ae49c0
moeyad7 <mohamed.eyad2612@gmail.com>
2023-04-22 16:38:58 +0200

    video 17 folder 10

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -142,7 +142,20 @@ class Products with ChangeNotifier {
   }
 
   void deleteProduct(String id) {
-    _items.removeWhere((prod) => prod.id == id);
+    final url = Uri.parse(
+        'https://flutter-shop-app-b1bc8-default-rtdb.firebaseio.com/products/$id.json');
+    final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
+    var existingProduct = _items[existingProductIndex];
+    http.delete(url).then((response) {
+      if(response.statusCode >= 400){
+        
+      }
+      existingProduct = null;
+    }).catchError((_) {
+      _items.insert(existingProductIndex, existingProduct);
+      notifyListeners();
+    });
+    _items.removeAt(existingProductIndex);
     notifyListeners();
   }
 }
