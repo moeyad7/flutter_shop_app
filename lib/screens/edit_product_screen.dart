89bb9c500189f75fb592d9729df4092aaad419b8
moeyad7 <mohamed.eyad2612@gmail.com>
2023-04-22 15:00:28 +0200

    video 11 folder 10

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -96,7 +96,21 @@ class _EditProductScreenState extends State<EditProductScreen> {
     } else {
       Provider.of<Products>(context, listen: false)
           .addProduct(_editedProduct)
-          .then((_) {
+          .catchError((error) {
+        return showDialog<Null>(
+            context: context,
+            builder: (ctx) => AlertDialog(
+                    title: Text('An Error occurred!'),
+                    content: Text('Something went wrong.'),
+                    actions: <Widget>[
+                      TextButton(
+                        child: Text('Okay'),
+                        onPressed: () {
+                          Navigator.of(ctx).pop();
+                        },
+                      )
+                    ]));
+      }).then((_) {
         setState(() {
           _isLoading = false;
         });
