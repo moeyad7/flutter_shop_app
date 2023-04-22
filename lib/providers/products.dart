78a9d6021b8a65f217f2b0e81dbadf8ed8c7b9ea
moeyad7 <mohamed.eyad2612@gmail.com>
2023-04-22 15:50:15 +0200

    video 16 folder 10

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -122,9 +122,18 @@ class Products with ChangeNotifier {
     }
   }
 
-  void updateProduct(String id, Product newProduct) {
+  Future<void> updateProduct(String id, Product newProduct) async {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
+      final url = Uri.parse(
+          'https://flutter-shop-app-b1bc8-default-rtdb.firebaseio.com/products/$id.json');
+      await http.patch(url,
+          body: json.encode({
+            'title': newProduct.title,
+            'description': newProduct.description,
+            'imageUrl': newProduct.imageUrl,
+            'price': newProduct.price,
+          }));
       _items[prodIndex] = newProduct;
       notifyListeners();
     } else {
