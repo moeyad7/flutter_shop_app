fe4a33f4c9351e76b52e541d94861348e4a8f932
moeyad7 <mohamed.eyad2612@gmail.com>
2023-04-25 17:12:24 +0200

    finished folder 12

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../helpers/custom_route.dart';
 import '../screens/orders_screen.dart';
 import '../screens/user_product_screen.dart';
 import '../providers/auth.dart';
@@ -30,6 +31,11 @@ class AppDrawer extends StatelessWidget {
             onTap: () {
               Navigator.of(context)
                   .pushReplacementNamed(OrdersScreen.routeName);
+              // Navigator.of(context).pushReplacement(
+              //   CustomeRoute(
+              //     builder: (ctx) => OrdersScreen(),
+              //   ),
+              // );
             },
           ),
           Divider(),
@@ -50,7 +56,7 @@ class AppDrawer extends StatelessWidget {
               Navigator.of(context).pushReplacementNamed('/');
               //   Navigator.of(context)
               //       .pushReplacementNamed(UserProductScreen.routeName);
-              Provider.of<Auth>(context,listen: false).logout();
+              Provider.of<Auth>(context, listen: false).logout();
             },
           ),
         ],
